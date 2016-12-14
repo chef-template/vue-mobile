0792487d989dbd08f76c1b3711480d254c0373e7
2046 <g@ggg.gd>
2016-12-14 17:46:42 +0800

    add not found route

--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,19 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-let router = new Router({
+let router, notFound
+
+notFound = {
+    path: '*',
+    meta: { title: '404' },
+    component: {render: (h) => h('h1', '404 not found')}
+}
+
+router = new Router({
     mode: 'history',
     routes: Object.keys(routes).reduce((previous, current) => {
         return (previous.push({path: current, ...routes[current]}), previous)
-    }, []),
+    }, []).concat(notFound),
     scrollBehavior(to, from, savedPosition) {
         return { x: 0, y: 0 }
     }
